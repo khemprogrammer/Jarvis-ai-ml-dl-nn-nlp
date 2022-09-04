270dfc2d6646f5c555f5a42801df9e5cc2351494
khem Bahadur Lodh <55244922+khemprogrammer@users.noreply.github.com>
2022-09-05 01:05:36 +0545

    Add files via upload

---
+++ Listen.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+
+def Listen():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,language="en-in")
+        print(f"You Said : {query}")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    query = str(query)
+    return query.lower()
+
+Listen()
